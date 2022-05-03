837993ccd05500c8c0a140d1a373972112256fa9
Eric <hayter.eric@gmail.com>
2022-05-03 09:37:03 -0400

    got rid of debugging prints

--- Steganography.py
+++ Steganography.py
@@ -21,17 +21,12 @@ def encrypt(img: Image, msg: str):
 
     # turning our message into a binary representation
     for c in msg:
-        bit_msg.append(bin(ord(c))[2:])
-
-    # TODO have some sort of issue here with encryption it looks like
-    print(bit_msg[0])
+        bit_msg.append(bin(ord(c))[2:].zfill(8))
 
     bit_msg = "".join(bit_msg)
 
     # spliting the binary string every two bits
     bit_msg = [bit_msg[i:i+2] for i in range(0, len(bit_msg), 2)]
-
-    print(bit_msg[0:n])
 
     # flatten for ease of use
     img_data = img_data.flatten()
@@ -54,13 +49,12 @@ def decrypt(img: Image) -> str:
     for p in img_data:
         img_chars.append(bin(p)[-2:])
         
-    print(img_chars[:n])
-
     img_chars = "".join(img_chars)
-    for i in range(0, len(img_chars), 7):
-        print(chr(int(img_chars[i:i+7],2)))
-        img_str += chr(int(img_chars[i:i+7],2))
-        if i > 70:
+
+    for i in range(0, (len(img_chars)//8)*8, 8):
+        try:
+            img_str += chr(int(img_chars[i:i+8],2))
+        except:
             break
 
     return img_str        
