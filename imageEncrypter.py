46ff26d03e1b0e6a64fc6fa0bbce8fc5c52b377f
Eric <hayter.eric@gmail.com>
2022-05-02 21:27:57 -0400

    more testing

--- imageEncrypter.py
+++ imageEncrypter.py
@@ -2,8 +2,8 @@ import Steganography
 
 def main():
     steg = Steganography.open("C:/Users/hayte/python_steganography/Balloon.jpg")
-    pixel_value = Steganography.encrypt(steg, "Hello world")[0]
-
+    img = Steganography.encrypt(steg, "Something else is weird")
+    print(Steganography.decrypt(img))
 
 if __name__ == "__main__":
     main()
