6708ade55d89c90a0a4387c77f34ff9d716f7fbd
Eric <hayter.eric@gmail.com>
2022-05-03 09:35:37 -0400

    more testing

--- imageEncrypter.py
+++ imageEncrypter.py
@@ -2,8 +2,9 @@ import Steganography
 
 def main():
     steg = Steganography.open("C:/Users/hayte/python_steganography/Balloon.jpg")
-    img = Steganography.encrypt(steg, "Something else is weird")
-    print(Steganography.decrypt(img))
+    img = Steganography.encrypt(steg, "Hello else is weird")
+    print(Steganography.decrypt(img)) # everything is working right now
+    # might want to fit in a automatic word finder not sure yet
 
 if __name__ == "__main__":
     main()
